0d35f8a4648b52851226759d8142111622413071
Otávio Augusto de Queiroz Reis <otavioreis@gmail.com>
2018-06-21 11:16:22 -0300

    Add project files.

---
+++ ApiMicrosservicos/Controllers/v1/pbl/PedidosController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Livraria.Api.Extensions;
+using Livraria.Api.ObjectModel;
+using Livraria.Api.ObjectModel.Swagger.Examples;
+using Livraria.Api.ObjectModel.ValueObjects;
+using Livraria.Api.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Examples;
+
+namespace Livraria.Api.Controllers.v1.pbl
+{
+    [Route("v1/public/[controller]")]
+    public class PedidosController : Controller
+    {
+        private readonly ILivrariaRepository _livrariaRespository;
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public PedidosController(ILivrariaRepository livrariaRepository, IPedidoRepository pedidoRepository)
+        {
+            _livrariaRespository = livrariaRepository;
+            _pedidoRepository = pedidoRepository;
+        }
+
+        /// <summary>
+        /// Cria um novo pedido de acordo com os itens de um carrinho de uma sessão
+        /// </summary>
+        /// <param name="sessionId">String a sessionId</param>
+        /// <response code="400">Bad Request</response>
+        [SwaggerResponseExample(200, typeof(PedidoExample))]
+        [HttpPost("{sessionId}")]
+        public async Task<IActionResult> CriarPedido(string sessionId)
+        {
+            var pedidoCriadoAnteriormente = await _pedidoRepository.GetPedidoPorSessionId(sessionId);
+
+            if (pedidoCriadoAnteriormente != null)
+                return BadRequest("Este pedido já havia sido criado anterirmente para esta sessionId");
+
+            var carrinho = HttpContext.Session.GetObject<List<ItemCarrinho>>(sessionId);
+
+            if (carrinho == null)
+                return BadRequest("Carrinho não encontrado.");
+
+            if (carrinho.Count < 1)
+                return BadRequest("Carrinho não possui itens.");
+
+            var livrosPedido = await _livrariaRespository.GetLivrosPorItensCarrinhoAsync(carrinho);
+
+            var novoPedido = new Pedido
+            {
+                Id = Guid.NewGuid(),
+                SessionId = sessionId,
+                Itens = livrosPedido,
+                ValorTotal = livrosPedido.Sum(l => l.ValorTotal),
+                Status = StatusPedido.EmAberto
+            };
+
+            var sucessoInsertPedido = await _pedidoRepository.InsertPedidoAsync(novoPedido);
+
+            if(sucessoInsertPedido)
+                return Ok(novoPedido);
+
+            return BadRequest("Erro ao criar novo pedido");
+        }
+
+        /// <summary>
+        /// Retorna os dados de um pedido de acordo com o seu id
+        /// </summary>
+        /// <param name="idPedido">Guid do id do Pedido</param>
+        /// <response code="400">Bad Request</response>
+        [SwaggerResponseExample(200, typeof(PedidoExample))]
+        [Route("{idPedido}")]
+        [HttpGet]
+        public async Task<IActionResult> GetDadosPedido(Guid idPedido)
+        {
+            var pedido = await _pedidoRepository.GetPedidoPorIdAsync(idPedido);
+
+            if (pedido != null)
+                return Ok(pedido);
+
+            return BadRequest("Pedido não encontrado");
+        }
+    }
+}
---
+++ ApiMicrosservicos/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Livraria.Api.Repository;
+using Livraria.Api.Repository.Interface;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Examples;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Livraria.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Livraria API",
+                    Description = "Um exemplo de endpoints de uma API de uma loja de livraria construída com ASP.NET Core Web API",
+                    Contact = new Contact
+                    {
+                        Name = "Otávio Augusto de Queiroz Reis",
+                        Email = "otavioreis@gmail.com",
+                        Url = String.Empty
+                    },
+                    License = new License
+                    {
+                        Name = "Licença MIT",
+                        Url = "https://raw.githubusercontent.com/otavioreis/ArquiteturaBackEndAula02Exercicio2/master/LICENSE"
+                    }
+                });
+                c.IncludeXmlComments(GetXmlCommentsPath());
+                c.OperationFilter<ExamplesOperationFilter>(); // [SwaggerRequestExample] & [SwaggerResponseExample]
+                c.OperationFilter<DescriptionOperationFilter>(); // [Description] on Response properties
+            });
+
+            services.AddMvc();
+            services.AddSession();
+            services.AddSingleton<ILivrariaRepository, LivrariaRepository>();
+            services.AddSingleton<IPedidoRepository, PedidoRepository>();
+        }
+
+        protected static string GetXmlCommentsPath()
+        {
+            return System.String.Format(@"{0}\Swagger.xml", System.AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Livraria API V1");
+            });
+
+            app.UseSession();
+            app.UseMvc();
+        }
+
+
+    }
+}
